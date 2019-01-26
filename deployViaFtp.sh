87801a0141e186971db3ef1a2c39ef0b367a15e1
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2019-01-26 15:17:37 -0600

    add deploy script fixes

--- deployViaFtp.sh
+++ deployViaFtp.sh
@@ -1,5 +1,10 @@
+# TODO setup host server with ssh to tranfer files easier
+
 echo open $HOST > ftp.txt
 echo user $USER $PASS >> ftp.txt
+echo mkdir static
+echo mkdir static/js
+echo mkdir static/css
 find ./build/ -type f -printf "put build/%P %P\n" >> ftp.txt
 echo bye >> ftp.txt
 
