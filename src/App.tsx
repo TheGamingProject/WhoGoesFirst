ce749f0b0106bf385aa85949c90b06e1fa7a94d4
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2019-01-26 14:08:18 -0600

    add initial work from stackblitz

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import { default as classNames } from 'classnames';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import './style.css';
+import './animation.css';
+import { getRandomQuestion } from './questions';
+
+interface AppProps { }
+interface AppState {
+  name: string;
+  displayedQuestion: string;
+  isFlipping: boolean;
+}
+
+export class App extends Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      name: 'React', 
+      displayedQuestion: getRandomQuestion(''),
+      isFlipping: false,
+    };
+  }
+
+  render() {
+
+    const onRerollClick = () => {
+      this.setState({
+        displayedQuestion: getRandomQuestion(this.state.displayedQuestion),
+        isFlipping: true,
+      });
+      setTimeout(() => {
+        this.setState({
+          isFlipping: false,
+        });
+      }, 800);
+    }
+
+    return (
+      <div className="app">
+        <div className="container"> 
+          <h1> The player who goes first is: </h1>
+          <div className={classNames('spinning-question', 'animate', {flipInX: this.state.isFlipping})}>
+            <div className="inner">
+              { this.state.displayedQuestion }
+            </div>
+          </div>
+          <button className="reroll-btn" onClick={onRerollClick} disabled={this.state.isFlipping}> Who goes first? </button>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/questions.tsx
@@ -0,0 +1,128 @@
+/**
+ * Filter ideas
+ *  - subjective (who has the best car)
+ * 
+ */
+
+export const questions = [
+  {
+  text: 'The player who has the nearest birthday'
+  },
+  {
+    text: 'The player to first sing a song lyric'
+  },
+  {
+    text: 'The player that last won a board game'
+  },
+  {
+    text: 'The player who most recently slept in a tent'
+  },
+  {
+    text: 'The player whose first name would be first in alphabetical order'
+  },
+  {
+    text: 'The player who most recently went to a music concert'
+  },
+  {
+    text: 'The player who most recently went to a convention'
+  },
+  {
+    text: 'The player who most recently was in a different city'
+  },
+  {
+    text: 'The player who has the nearest birthday'
+  },
+  {
+    text: 'The player currently reading this'
+  },
+  {
+    text: 'The player with the longest hair'
+  },
+  {
+    text: 'The player with the shortest hair'
+  },
+  {
+    text: 'The player closest to the rule book'
+  },
+  {
+    text: 'The player to the left of the player reading this'
+  },
+  {
+    text: 'The player to the right of the player reading this'
+  },
+  {
+    text: 'The player who hasn\'t played board games the longest'
+  },
+  {
+    text: 'The player who last played pinball'
+  },
+  {
+    text: 'The player who last walked a dog'
+  },
+  {
+    text: 'The player who most recently ate eggs'
+  },
+  {
+    text: 'The player who was last in a foreign country'
+  },
+  {
+    text: 'The player who last listened to a podcast'
+  },
+  {
+    text: 'The player with the largest phone'
+  },
+  {
+    text: 'The player with the smallest phone'
+  },
+  {
+    text: 'The most colorfully dressed player'
+  },
+  {
+    text: 'The player who last built a campfire'
+  },
+  {
+    text: 'The player who most recently watched a movie'
+  },
+  {
+    text: 'The player most recently inside of a school'
+  },
+  {
+    text: 'The player who last changed a diaper'
+  },
+  {
+    text: 'The first player to give a huge smile'
+  },
+  {
+    text: 'The player who was last on a farm'
+  },
+  {
+    text: 'The player who last drank coffee'
+  },
+  {
+    text: 'The player who last drank tea'
+  },
+  {
+    text: 'The player who has gone the longest without coffee'
+  },
+  {
+    text: 'The player who has gone the longest without tea'
+  },
+  {
+    text: 'The player who was born the farthest away'
+  },
+  {
+    text: 'The player who last watched a movie in a theatre'
+  },
+];
+
+export function getRandomQuestion(previousQuestion?: string): string {
+  let question: string; 
+  do {
+    question = questions[getRandomNumber(0, questions.length - 1)].text
+  } while(question === previousQuestion);
+  return question;
+}
+
+function getRandomNumber(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
